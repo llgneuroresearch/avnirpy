ba67d50c1a1dbf8b57c0c1cf92e1ce82134db8f5
Laurent Letourneau-Guillon <48111184+laurentletg@users.noreply.github.com>
2024-12-09 14:15:54 -0500

    Delete scripts/avnir_check_number_of_files directory
    removed unnecessary directory

--- scripts/avnir_check_number_of_files/llg_ggtv.sh
+++
@@ -1,4 +0,0 @@
-python3 avnir_check_number_vols_vs_segs.py \
-    --input_directory_volumes "/Users/laurentletourneau-guillon/Dropbox (Personal)/CHUM/RECHERCHE/2020BahigENE/GGGTV_Bahig/GGGTV_Bahig/test_segmentation/Patients_to_annotate/slicer_format_vols" \
-    --input_directory_segmentations "/Users/laurentletourneau-guillon/Dropbox (Personal)/CHUM/RECHERCHE/2020BahigENE/GGGTV_Bahig/GGGTV_Bahig/test_segmentation/Patients_to_annotate/slicer_format_preds" \
-    --log_level "DEBUG"
